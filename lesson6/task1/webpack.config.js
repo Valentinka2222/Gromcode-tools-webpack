077e6a7440d83d4e1767c3d67a32f6b024708df4
Valentinka2222 <xamalyan89@gmail.com>
2022-05-07 18:00:11 +0300

    L6t1

---
+++ lesson6/task1/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|png)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
