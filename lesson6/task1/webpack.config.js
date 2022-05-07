c1f60a440b7e30b7613f046b3ddbb0a10766780a
Valentinka2222 <xamalyan89@gmail.com>
2022-05-07 18:04:07 +0300

    1

--- lesson6/task1/webpack.config.js
+++ lesson6/task1/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: '/.s?css$/',
+        test: /\.s?css$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
